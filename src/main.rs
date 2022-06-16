0554637173e5838aab2d4c2c99b487b7c624df5f
FHLC-FUND <88232269+FHLC-FUND@users.noreply.github.com>
2022-06-16 08:51:13 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,40 @@ fn main() {
     let sub_arr = [1,8,10];
     let mut index_org = 0;
     let mut result = false;
+    if sub_arr.len() == 0 || org_arr.len() == 0 {
+        println!("Chuỗi cần so sánh = 0")
+    }
+    else if sub_arr > org_arr{
+        println!("Chuỗi con lớn hơn chuỗi bố")
+    }
+    else{
+        while index_org < org_arr.len() {
+            let mut index_sub=0;
 
-    while index_org < org_arr.len() {
-        let mut index_sub=0;
-
-        if org_arr[index_org]==sub_arr[index_sub]{
-            let mut i = index_org;
-            while i < org_arr.len()&&index_sub<sub_arr.len(){
-                if org_arr[i]==sub_arr[index_sub] {
-                    result = true;
-                    i+=1;
-                    index_sub+=1;
-                }else{
-                    index_sub=0;
-                    result = false;
-                    break;
+            if org_arr[index_org]==sub_arr[index_sub]{
+                let mut i = index_org;
+                while i < org_arr.len()&&index_sub<sub_arr.len(){
+                    if org_arr[i]==sub_arr[index_sub] {
+                        result = true;
+                        i+=1;
+                        index_sub+=1;
+                    }else{
+                        index_sub=0;
+                        result = false;
+                        break;
+                    }
                 }
             }
+            if result {
+                break;
+            }
+            index_org += 1;
         }
-        if result {
-            break;
+        if result == true{
+            println!("Là chuỗi con");
+        }else {
+            println!("Không phải chuỗi con")
         }
-        index_org += 1;
-    }
-    if result == true{
-        println!("Là chuỗi con");
-    }else {
-        println!("Không phải chuỗi con")
     }
     //
     let mut line = String::new();
